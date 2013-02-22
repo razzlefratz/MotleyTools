42157af52c98772ea15362e96da40614f189b52b
Charles Maier <charlesmaier@localhost>
2013-02-22 05:16:54 -0500

    Adding old socket functions

---
+++ gcpp/socket/Client.cpp
@@ -0,0 +1,59 @@
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+#ifndef CLIENT_SOURCE
+#define CLIENT_SOURCE
+
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+#include <string.h>
+
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+#include "ServerSocket.h"
+#include "SocketException.h"
+
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+int main (int argc, int argv []) 
+
+{
+	std::cout << "running....\n";
+	try 
+	{
+		ServerSocket server (30000);
+		while (true) 
+		{
+			ServerSocket new_sock;
+			server.accept (new_sock);
+			try 
+			{
+				while (true) 
+				{
+					std::string data;
+					new_sock >> data;
+					new_sock << data;
+				}
+			}
+			catch (SocketException &) 
+			{
+			}
+		}
+	}
+	catch (SocketException & e) 
+	{
+		std::cout << "Exception was caught:" << e.description () << "\nExiting.\n";
+	}
+	exit (0);
+}
+
+
+#endif
+
---
+++ gcpp/socket/Server.cpp
@@ -0,0 +1,59 @@
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+#ifndef SERVER_SOURCE
+#define SERVER_SOURCE
+
+/*====================================================================*
+ *   system header files;
+ *--------------------------------------------------------------------*/
+
+#include <string.h>
+
+/*====================================================================*
+ *   custom header files;
+ *--------------------------------------------------------------------*/
+
+#include "ServerSocket.h"
+#include "SocketException.h"
+
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+int main (int argc, int argv []) 
+
+{
+	std::cout << "running....\n";
+	try 
+	{
+		ServerSocket server (30000);
+		while (true) 
+		{
+			ServerSocket new_sock;
+			server.accept (new_sock);
+			try 
+			{
+				while (true) 
+				{
+					std::string data;
+					new_sock >> data;
+					new_sock << data;
+				}
+			}
+			catch (SocketException &) 
+			{
+			}
+		}
+	}
+	catch (SocketException & e) 
+	{
+		std::cout << "Exception was caught:" << e.description () << "\nExiting.\n";
+	}
+	exit (0);
+}
+
+
+#endif
+
