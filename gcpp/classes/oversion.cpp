8e7505a9eb86e4fbf6e472fc8158b5ade058fa55
Charlie Maier <razzlefratz@gmail.com>
2013-03-24 19:40:49 -0400

    Update selected comments.

--- gcpp/classes/oversion.cpp
+++ gcpp/classes/oversion.cpp
@@ -2,8 +2,6 @@
  *
  *   oversion.cpp - implementation of the oversion class.
  *
- *   this class
- *
  *.  Motley Tools by Charles Maier
  *:  Published 1982-2005 by Charles Maier for personal use
  *;  Licensed under the Internet Software Consortium License
@@ -27,37 +25,217 @@
 
 /*====================================================================*
  *
+ *   oversion & program (char const * program);
+ *   char const * program () const * ;
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+
+oversion & oversion::program (char const * program) 
+{
+	this->mprogram = otext::replace (this->mprogram, program);
+	return (*this);
+}
+
+char const * oversion::program () const
+{
+	return (this->mprogram);
+}
+
+
+/*====================================================================*
+ *
+ *   oversion & company (char const * company);
+ *   char const * company () const * ;
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+
+oversion & oversion::company (char const * company) 
+{
+	this->mcompany = otext::replace (this->mcompany, company);
+	return (*this);
+}
+
+char const * oversion::company () const
+{
+	return (this->mcompany);
+}
+
+
+/*====================================================================*
+ *
+ *   oversion & project (char const * project);
+ *   char const * project () const * ;
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+
+oversion & oversion::project (char const * project) 
+{
+	this->mproject = otext::replace (this->mproject, project);
+	return (*this);
+}
+
+char const * oversion::project () const
+{
+	return (this->mproject);
+}
+
+
+/*====================================================================*
+ *
+ *   oversion & package (char const * package);
+ *   char const * package () const * ;
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+
+oversion & oversion::package (char const * package) 
+{
+	this->mpackage = otext::replace (this->mpackage, package);
+	return (*this);
+}
+
+char const * oversion::package () const
+{
+	return (this->mpackage);
+}
+
+
+/*====================================================================*
+ *
+ *   char const * version () const * ;
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+
+oversion & oversion::version (char const * version) 
+{
+	this->mversion = otext::replace (this->mversion, version);
+	return (*this);
+}
+
+char const * oversion::version () const
+{
+	return (this->mversion);
+}
+
+
+/*====================================================================*
+ *
+ *   oversion & release (char const * release);
+ *   char const * release () const * ;
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+
+oversion & oversion::release (char const * release) 
+{
+	this->mrelease = otext::replace (this->mrelease, release);
+	return (*this);
+}
+
+char const * oversion::release () const
+{
+	return (this->mrelease);
+}
+
+
+/*====================================================================*
+ *
+ *   oversion & contact (char const * contact);
+ *   char const * contact () const * ;
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+
+oversion & oversion::contact (char const * contact) 
+{
+	this->mcontact = otext::replace (this->mcontact, contact);
+	return (*this);
+}
+
+char const * oversion::contact () const
+{
+	return (this->mcontact);
+}
+
+
+/*====================================================================*
+ *
  *   oversion & print (void);
  *
  *   print full version information on stdout;
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
  *
  *--------------------------------------------------------------------*/
 
 oversion & oversion::print (void) 
 
 {
-	extern char const * program_name;
-	std::cerr << program_name << " ";
-	std::cerr << PACKAGE << "-";
-	std::cerr << VERSION << ".";
-	std::cerr << RELEASE << " ";
+	std::cerr << this->mprogram << " ";
+	std::cerr << this->mpackage << "-";
+	std::cerr << this->mversion << " ";
 	std::cerr << "C++" << " ";
-	std::cerr << COMPANY << " ";
-	std::cerr << __TIME__ << " ";
-	std::cerr << __DATE__ << std::endl;
+	std::cerr << this->mcompany << " ";
+	std::cerr << this->mrelease << " ";
+	std::cerr << std::endl;
 	return (*this);
 }
-
 
 /*====================================================================*
  *
  *   oversion ();
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
  *
  *--------------------------------------------------------------------*/
 
 oversion::oversion () 
 
 {
+	this->mprogram = otext::save (PROGRAM);
+	this->mcompany = otext::save (COMPANY);
+	this->mproject = otext::save (PROJECT);
+	this->mpackage = otext::save (PACKAGE);
+	this->mversion = otext::save (VERSION);
+	this->mrelease = otext::save (RELEASE);
+	this->mlicense = otext::save (LICENSE);
+	this->mcontact = otext::save (CONTACT);
 	return;
 }
 
@@ -65,6 +243,10 @@ oversion::oversion ()
 /*====================================================================*
  *   
  *   ~oversion ();
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
  *
  *--------------------------------------------------------------------*/
 
