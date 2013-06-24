57576268a29949e003c81aa4ed0e6962257ed85d
Charles Maier on Appslinux01 <cmaier@qca.qualcomm.com>
2013-06-24 01:32:57 -0400

    Modified program cbrief to print only comments, as well.

--- gcc/cmassoc/cbrief.c
+++ gcc/cmassoc/cbrief.c
@@ -41,17 +41,68 @@
 #endif
 
 #ifndef MAKEFILE
-#include "../tidy/nocomment.c"
 #include "../tidy/literal.c"
+#include "../tidy/nocomment.c"
+#include "../tidy/noliteral.c"
+#include "../tidy/noescaped.c"
 #include "../tidy/escaped.c"
 #include "../tidy/keep.c"
 #endif
 
 /*====================================================================*
  *
- *   void function (flag_t flags);
+ *   void comment (int c);
  *
- *   read stdin and write stdout; intercept and discard C Language
+ *   tidy.h
+ *
+ *   read stdin and write stdout; pass C style comment block;
+ *
+ *.  Motley Tools by Charles Maier
+ *:  Published 1982-2005 by Charles Maier for personal use
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+signed comment (signed c) 
+
+{
+	c = getc (stdin);
+	if (c == '/') 
+	{
+		putc ('/', stdout);
+		do 
+		{
+			c = keep (c);
+		}
+		while ((c != '\n') && (c != EOF));
+		putc ('\n', stdout);
+		c = keep (c);
+		return (c);
+	}
+	if (c == '*') 
+	{
+		putc ('/', stdout);
+		c = keep (c);
+		while ((c != '/') && (c != EOF)) 
+		{
+			while ((c != '*') && (c != EOF)) 
+			{
+				c = keep (c);
+			}
+			c = keep (c);
+		}
+		c = keep (c);
+		putc ('\n', stdout);
+		return (c);
+	}
+	return (c);
+}
+
+/*====================================================================*
+ *
+ *   void exclude (void);
+ *
+ *   read stdin and write stdout; intercept and exclude C Language
  *   style comments that start with double slash or appear between
  *   inverted slash/asterisk character pairs;
  *
@@ -61,9 +112,10 @@
  *
  *--------------------------------------------------------------------*/
 
-void function (signed c, flag_t flags) 
+static void exclude (void) 
 
 {
+	signed c = getc (stdin);
 	while (c != EOF) 
 	{
 		if (c == '/') 
@@ -83,6 +135,68 @@ void function (signed c, flag_t flags)
 
 /*====================================================================*
  *
+ *   void include (void);
+ *
+ *   read stdin and write stdout; intercept and include C Language
+ *   style comments that start with double slash or appear between
+ *   inverted slash/asterisk character pairs;
+ *
+ *.  Motley Tools by Charles Maier;
+ *:  Copyright (c) 2001-2006 by Charles Maier Associates Limited;
+ *;  Licensed under the Internet Software Consortium License;
+ *
+ *--------------------------------------------------------------------*/
+
+static void include (void) 
+
+{
+	signed c = getc (stdin);
+	while (c != EOF) 
+	{
+		if (c == '/') 
+		{
+			c = getc (stdin);
+			if (c == '/') 
+			{
+				putc ('/', stdout);
+				do 
+				{
+					c = keep (c);
+				}
+				while ((c != '\n') && (c != EOF));
+				putc ('\n', stdout);
+				c = keep (c);
+				continue;
+			}
+			if (c == '*') 
+			{
+				putc ('/', stdout);
+				c = keep (c);
+				while ((c != '/') && (c != EOF)) 
+				{
+					while ((c != '*') && (c != EOF)) 
+					{
+						c = keep (c);
+					}
+					c = keep (c);
+				}
+				c = keep (c);
+				putc ('\n', stdout);
+				continue;
+			}
+		}
+		if (isquote (c)) 
+		{
+			c = noliteral (c);
+			continue;
+		}
+		c = getc (stdin);
+	}
+	return;
+}
+
+/*====================================================================*
+ *
  *   int main (int argc, char const * argv []);
  *
  *.  Motley Tools by Charles Maier;
@@ -96,17 +210,21 @@ int main (int argc, char const * argv [])
 {
 	static char const * optv [] = 
 	{
-		"",
+		"v",
 		PUTOPTV_S_FILTER,
 		"remove C/C++ language comments",
+		"v\tdiscard source code",
 		(char const *) (0)
 	};
-	flag_t flags = (flag_t) (0);
+	void (* function) (void) = exclude;
 	signed c;
 	while ((c = getoptv (argc, argv, optv)) != -1) 
 	{
 		switch (c) 
 		{
+		case 'v':
+			function = include;
+			break;
 		default:
 			break;
 		}
@@ -115,13 +233,13 @@ int main (int argc, char const * argv [])
 	argv+= optind;
 	if (!argc) 
 	{
-		function (getc (stdin), flags);
+		function ();
 	}
 	while ((argc) && (* argv)) 
 	{
 		if (vfopen (* argv)) 
 		{
-			function (getc (stdin), flags);
+			function ();
 		}
 		argv++;
 		argc--;
