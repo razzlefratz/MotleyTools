be0acc336a5454fbf77014d72bafd966ba2f7546
Charles Maier on Appslinux01 <cmaier@qca.qualcomm.com>
2013-08-11 22:37:01 -0400

    Add new program script

---
+++ gcpp/programs/script.cpp
@@ -0,0 +1,112 @@
+/*====================================================================*
+ *
+ *   cfm.cpp - format C/C++ source files;
+ *
+ *   custom C/C++ source code formatter;
+ *
+ *.  Motley Tools by Charles Maier <cmaier@cmassoc.net>;
+ *:  Copyright 2001-2006 by Charles Maier Associates;
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+/*====================================================================*
+ *   system header files;
+ *--------------------------------------------------------------------*/
+
+#include <cstdlib>
+#include <iostream>
+
+/*====================================================================*
+ *   custom header files;
+ *--------------------------------------------------------------------*/
+
+#include "../classes/ogetoptv.hpp"
+#include "../classes/ofileopen.hpp"
+#include "../classes/opathspec.hpp"
+#include "../classes/oprofile.hpp"
+
+/*====================================================================*
+ *   custom source files;
+ *--------------------------------------------------------------------*/
+
+#ifndef MAKEFILE
+#include "../classes/ogetoptv.cpp"
+#include "../classes/oputoptv.cpp"
+#include "../classes/oversion.cpp"
+#include "../classes/oerror.cpp"
+#include "../classes/oflagword.cpp"
+#include "../classes/ofileopen.cpp"
+#include "../classes/ofilespec.cpp"
+#include "../classes/okeywords.cpp"
+#include "../classes/opathspec.cpp"
+#include "../classes/oprofile.cpp"
+#include "../classes/ocontext.cpp"
+#include "../classes/owildcard.cpp"
+#include "../classes/oescape.cpp"
+#include "../classes/oswitch.cpp"
+#include "../classes/otext.cpp"
+#include "../classes/oascii.cpp"
+#include "../classes/oscript.cpp"
+#endif
+
+/*====================================================================*
+ *   program constants;
+ *--------------------------------------------------------------------*/
+
+#define PROFILE_NAME "/etc/cfm.ini"     
+#define SECTION_NAME "default"     
+
+/*====================================================================*
+ *   
+ *   int main (int argc, char const * argv []);
+ *   
+ *   
+ *.  Motley Tools by Charles Maier <cmaier@cmassoc.net>;
+ *:  Copyright 2001-2006 by Charles Maier Associates;
+ *;  Licensed under the Internet Software Consortium License
+ *
+ *--------------------------------------------------------------------*/
+
+int main (int argc, char const * argv []) 
+
+{ 
+	static char const * optv [] = 
+	{ 
+		"", 
+		oPUTOPTV_S_FILTER, 
+		"format script files", 
+		"f s\tuse profile (s) [" LITERAL (PROFILE_NAME) "]", 
+		"g s\tuse profile section (s) [" LITERAL (SECTION_NAME) "]", 
+		(char const *) (0)
+	}; 
+	ogetoptv getopt; 
+	opathspec pathspec; 
+	ofileopen fileopen; 
+	signed (oscript::* method) (signed) = & oscript::program; 
+	signed c; 
+	while (~ (c = getopt.getoptv (argc, argv, optv))) 
+	{ 
+		switch (c) 
+		{ 
+		default: 
+			break; 
+		} 
+	} 
+	if (!getopt.argc ()) 
+	{ 
+		(object.* method) (std::cin.get ()); 
+	} 
+	while (getopt.argc () && * getopt.argv ()) 
+	{ 
+		if (fileopen.openedit (* getopt.argv ())) 
+		{ 
+			object.filename (* getopt.argv ()); 
+			(object.* method) (std::cin.get ()); 
+			fileopen.close (); 
+		} 
+		getopt++; 
+	} 
+	exit (0); 
+} 
+
