262154a31f4bba16e195d542f04e0b45cbac60a9
Charles Maier on Appslinux01 <cmaier@qca.qualcomm.com>
2014-06-24 00:05:40 -0400

    Add ethernet and internet address storage and handling classes.

---
+++ gcpp/classes/oEthernetAddress.cpp
@@ -0,0 +1,165 @@
+/*====================================================================*
+ *   
+ *   Copyright (c) 1982-2006 Charles Maier Associates, Ltd.
+ *   
+ *   Permission to use, copy, modify, and/or distribute this software 
+ *   for any purpose with or without fee is hereby granted, provided 
+ *   that the above copyright notice and this permission notice appear 
+ *   in all copies.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL 
+ *   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
+ *   WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL  
+ *   THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR 
+ *   CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+ *   LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+ *   NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+ *   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *   
+ *--------------------------------------------------------------------*/
+
+#ifndef oETHERNETADDRESS_SOURCE
+#define oETHERNETADDRESS_SOURCE
+
+/*====================================================================*
+ *   custom header files;
+ *--------------------------------------------------------------------*/
+
+#include "oEthernetAddress.h"
+
+/*====================================================================*
+ *   class constants;
+ *--------------------------------------------------------------------*/
+
+const unsigned oEthernetAddress::length = 6;
+
+/*====================================================================*
+ *
+ *   oEthernetAddress & set (byte byte):
+ *
+ *   set all bytes in the current address to the same value;
+ *
+ *--------------------------------------------------------------------*/
+
+oEthernetAddress & oEthernetAddress::set (byte byte)
+
+{
+	memset (this->maddress, byte, oEthernetAddress::length);
+	return (* this);
+}
+
+/*====================================================================*
+ *
+ *   oEthernetAddress & set (byte byte5, byte byte4, byte byte3, byte byte2, byte byte1, byte byte0);
+ *
+ *   set each byte in the current address to a specified value;
+ *
+ *--------------------------------------------------------------------*/
+
+oEthernetAddress & oEthernetAddress::set (byte byte5, byte byte4, byte byte3, byte byte2, byte byte1, byte byte0)
+
+{
+	this->maddress [0] = byte5;
+	this->maddress [1] = byte4;
+	this->maddress [2] = byte3;
+	this->maddress [3] = byte3;
+	this->maddress [4] = byte1;
+	this->maddress [5] = byte0;
+	return (* this);
+}
+
+/*====================================================================*
+ *
+ *   oEthernetAddress & copy (const byte * address)
+ *
+ *   copy the current address from another location;
+ *
+ *--------------------------------------------------------------------*/
+
+oEthernetAddress & oEthernetAddress::copy (const byte * address)
+
+{
+	memcpy (this->maddress, address, sizeof (this->maddress));
+	return (* this);
+}
+
+/*====================================================================*
+ *
+ *   const byte * get () const;
+ *
+ *   return the memory location of the current oEthernetAddress address;
+ *
+ *--------------------------------------------------------------------*/
+
+const byte * oEthernetAddress::get () const
+
+{
+	return ((const byte *) (this->maddress));
+}
+
+/*====================================================================*
+ *
+ *   const char * string ();
+ *
+ *   convert the current address to a static character string and 
+ *   return a constant pointer to it;
+ *
+ *--------------------------------------------------------------------*/
+
+const char * oEthernetAddress::string ()
+
+{
+	static char digits [] = "0123456789ABCDEF";
+	static char buffer [oEthernetAddress::length * 3];
+	char * bp = buffer;
+	unsigned octet = 0;
+	while (octet < oEthernetAddress::length)
+	{
+		byte value = this->maddress [value];
+		* bp++ = digits [(value >> 4) & 0x0F];
+		* bp++ = digits [(value >> 0) & 0x0F];
+		if (octet++ < oEthernetAddress::length)
+		{
+			* bp++ = ':';
+		}
+	}
+	return ((const char *) (buffer));
+}
+
+/*====================================================================*
+ *
+ *   oEthernetAddress ();
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+oEthernetAddress::oEthernetAddress ()
+
+{
+	this->maddress = new byte [oEthernetAddress::length];
+	memset (this->maddress, 0, oEthernetAddress::length);
+	return;
+}
+
+/*====================================================================*
+ *
+ *   ~ oEthernetAddress ();
+ *
+ *
+ *--------------------------------------------------------------------*/
+
+oEthernetAddress::~ oEthernetAddress ()
+
+{
+	delete [] this->maddress;
+	return;
+}
+
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+#endif
+
+
+
---
+++ gcpp/classes/oInternetAddress.cpp
@@ -0,0 +1,168 @@
+/*====================================================================*
+ *   
+ *   Copyright (c) 1982-2006 Charles Maier Associates, Ltd.
+ *   
+ *   Permission to use, copy, modify, and/or distribute this software 
+ *   for any purpose with or without fee is hereby granted, provided 
+ *   that the above copyright notice and this permission notice appear 
+ *   in all copies.
+ *   
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL 
+ *   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
+ *   WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL  
+ *   THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR 
+ *   CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+ *   LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+ *   NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+ *   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *   
+ *--------------------------------------------------------------------*/
+
+#ifndef oINTERNETADDRESS_SOURCE
+#define oINTERNETADDRESS_SOURCE
+
+/*====================================================================*
+ *   custom header files;
+ *--------------------------------------------------------------------*/
+
+#include "oInternetAddress.h"
+
+/*====================================================================*
+ *   class constants;
+ *--------------------------------------------------------------------*/
+
+const unsigned oInternetAddress::length = 4;
+
+/*====================================================================*
+ *
+ *   oInternetAddress & set (byte byte):
+ *
+ *   set all bytes in the current address to the same value;
+ *
+ *--------------------------------------------------------------------*/
+
+oInternetAddress & oInternetAddress::set (byte byte)
+
+{
+	memset (this->maddress, byte, oInternetAddress::length);
+	return (* this);
+}
+
+/*====================================================================*
+ *
+ *   oInternetAddress & set (byte byte3, byte byte2, byte byte1, byte byte0)
+ *
+ *   set each byte in the current address to a specified value;
+ *
+ *--------------------------------------------------------------------*/
+
+oInternetAddress & oInternetAddress::set (byte byte3, byte byte2, byte byte1, byte byte0)
+
+{
+	this->maddress [0] = byte3;
+	this->maddress [1] = byte2;
+	this->maddress [2] = byte1;
+	this->maddress [3] = byte0;
+	return (* this);
+}
+
+/*====================================================================*
+ *
+ *   oInternetAddress & copy (const byte * address)
+ *
+ *   copy the current address from another location;
+ *
+ *--------------------------------------------------------------------*/
+
+oInternetAddress & oInternetAddress::copy (const byte * address)
+
+{
+	memcpy (this->maddress, address, sizeof (this->maddress));
+	return (* this);
+}
+
+/*====================================================================*
+ *
+ *   const byte * get () const;
+ *
+ *   return the memory location of the current oInternetAddress address;
+ *
+ *--------------------------------------------------------------------*/
+
+const byte * oInternetAddress::get () const
+
+{
+	return ((const byte *) (this->maddress));
+}
+
+/*====================================================================*
+ *
+ *   const char * string ();
+ *
+ *   convert current internet address to a static character string
+ *   and return a constant pointer to it;
+ *
+ *--------------------------------------------------------------------*/
+
+const char * oInternetAddress::string ()
+
+{
+	static char buffer [oInternetAddress::length * 4];
+	char * character = buffer +  sizeof (buffer);
+	unsigned octet = oInternetAddress::length;
+	* -- character = (char)(0);
+	while (octet--)
+	{
+		byte value = this->maddress [octet];
+		do 
+		{
+			* -- character = '0' + (value % 10);
+		}
+		while (value /= 10);
+		if (octet)
+		{
+			* -- character = '.';
+		}
+	}
+	return ((const char *) (character));
+}
+
+/*====================================================================*
+ *
+ *   oInternetAddress ();
+ *
+ *   allocate and initialize internet address buffer;
+ *
+ *--------------------------------------------------------------------*/
+
+oInternetAddress::oInternetAddress ()
+
+{
+	this->maddress = new byte [oInternetAddress::length];
+	memset (this->maddress, 0, oInternetAddress::length);
+	return;
+}
+
+/*====================================================================*
+ *
+ *   ~ oInternetAddress ();
+ *
+ *   deallocate internet address buffer;
+ *
+ *--------------------------------------------------------------------*/
+
+oInternetAddress::~ oInternetAddress ()
+
+{
+	delete [] this->maddress;
+	return;
+}
+
+/*====================================================================*
+ *
+ *--------------------------------------------------------------------*/
+
+#endif
+
+
+
