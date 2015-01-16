f06369916f536dca2b322f9aaf11df4e1373bf7a
Charles Maier on dazzle <razzlefratz@gmail.com>
2015-01-16 11:37:38 -0800

    types.h: Added huge_t data type and removed redundant byte_t declaration;

--- gcc/tools/types.h
+++ gcc/tools/types.h
@@ -90,10 +90,10 @@ typedef signed file_t;
 typedef signed sock_t;
 typedef signed code_t;
 typedef signed bool_t;
+typedef unsigned long long huge_t
 typedef unsigned char byte_t
 typedef unsigned char char_t
 typedef unsigned type_t;
-typedef unsigned char byte_t;
 typedef unsigned flag_t;
 typedef struct _file_
 
